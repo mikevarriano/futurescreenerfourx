6cd3838a11d8830b70a5ab2abdbd6ad0e1ed4a75
mikevarriano <mikev9000@yahoo.com>
2024-03-11 13:37:57 -0400

    Update test.py
    more stuff added

--- test.py
+++ test.py
@@ -2,11 +2,11 @@ import yfinance as yfinance
 
 #fghv is find high volume dates
 
-def fghv(symbol, period ="1d", interval= "1d", vm= 4 )
+def fghv(symbol, period ="1d", interval= "1d" )
     
 #fetch historical data
     
-data = yfinance.download(symbol, period= period,interval= interval )
+data = yfinance.download("^ES", period= "1d",interval= "1d" )
 
 #calculate average volume
 
@@ -14,4 +14,12 @@ avgvol = data['Volume'].mean()
 
 #find dates where volume is above average by specified multiple
 #hvd = high volume dates
+
+
+hvd = data[data['Volume'] >= avgvol * 4].index
+
+symbol = "^ES"
+
+hvd = fghv(symbol)
+
 
